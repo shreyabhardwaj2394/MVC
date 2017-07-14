6dfb9749e09a2664b3af9ba507e1f62341e16e53
shreyabhardwaj2394 <shreya2394bhardwaj@gmail.com>
2017-07-14 21:29:33 +0530

    Q4

--- src/main/java/com/exercise/StudentController.java
+++ src/main/java/com/exercise/StudentController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
@@ -17,13 +18,10 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class StudentController
 {
-
-    @RequestMapping(value = "/index.html", method = RequestMethod.GET)
-    public String printWelcome(ModelMap model) {
-        model.addAttribute("message", "HELLO WORLD FROM JSP");
-        return "index";
-
-    }
-
+        @RequestMapping("/index.html")
+        @ResponseBody
+        public String action() {
+            return "Hello World";
+        }
 
 }
