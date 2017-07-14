47d32ffbbb35e859dc0d850c25bea95a4d7f9884
shreyabhardwaj2394 <shreya2394bhardwaj@gmail.com>
2017-07-14 23:00:46 +0530

    Q2

--- src/main/java/com/exercise/StudentController.java
+++ src/main/java/com/exercise/StudentController.java
@@ -1,7 +1,9 @@
 package com.exercise;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,11 +11,18 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Created by Shreya on 7/14/2017.
  */
-public class StudentController extends AbstractController {
-    @Override
-    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
-        ModelAndView model=new ModelAndView("index");
-        model.addObject("message","HELLO FROM INDEX.JSP");
-        return model;
+public class StudentController extends MultiActionController {
+    @RequestMapping(value = "/index.html")
+    public ModelAndView dummy() {
+        ModelAndView modelAndView=new ModelAndView("index");
+        return modelAndView;
+
     }
+    @RequestMapping(value = "/two")
+    public void dummy2(HttpServletRequest httpServletRequest,
+                       HttpServletResponse httpServletResponse) throws Exception {
+        httpServletResponse.setContentType("text/html");
+        httpServletResponse.getWriter().println("<b>HELLO World</b>");
+    }
+
 }
